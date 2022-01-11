5bee4acbd62c4a5d2dfc06909c4a485be504a87f
Ahmed Ezz <es-ahmed.ezkak24a@alexu.edu.eg>
2022-01-11 23:57:31 +0200

    Create MachinetoJSON.java

---
+++ backend/producerconsumer/src/main/java/com/example/producerconsumer/MachinetoJSON.java
@@ -0,0 +1,30 @@
+package com.example.producerconsumer;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+
+public class MachinetoJSON {
+    ArrayList<Machine> machines = new ArrayList<>();
+
+    public MachinetoJSON(ArrayList<Machine> machines) {
+        this.machines = machines;
+    }
+
+    JSONArray GetJSON(){
+        JSONArray jsonArray = new JSONArray();
+        JSONObject jsonObject = new JSONObject();
+        for (int i = 0;i < machines.size();i++){
+            jsonObject.put("id", machines.get(i).getId());
+            jsonObject.put("color", machines.get(i).getColor());
+            jsonObject.put("next", machines.get(i).getNext().getId());
+            jsonObject.put("prev", machines.get(i).getPrev());
+            JSONObject JO = new JSONObject();
+            JO.put("x", machines.get(i).getPosition().getX());
+            JO.put("y", machines.get(i).getPosition().getY());
+            jsonObject.put("position", JO.toString());
+        }
+        return jsonArray;
+    }
+}
