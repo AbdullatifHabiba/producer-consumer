ad1f0b02e235593ce16787b416dc8df8ca942253
Ahmed Ezz <es-ahmed.ezkak24a@alexu.edu.eg>
2022-01-11 17:34:52 +0200

    lastlast

--- backend/producerconsumer/src/main/java/com/example/producerconsumer/QueueofProducts.java
+++ backend/producerconsumer/src/main/java/com/example/producerconsumer/QueueofProducts.java
@@ -17,7 +17,107 @@ public class QueueofProducts implements Observer {
         Id = id;
         Position = position;
         NumOfProducts = 0;
-        Products = new Queue<>();
+        Products = new Queue<>() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return false;
+            }
+
+            @Override
+            public Iterator<Product> iterator() {
+                return null;
+            }
+
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @Override
+            public <T> T[] toArray(T[] a) {
+                return null;
+            }
+
+            @Override
+            public boolean add(Product product) {
+                return false;
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(Collection<? extends Product> c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                return false;
+            }
+
+            @Override
+            public int hashCode() {
+                return 0;
+            }
+
+            @Override
+            public boolean offer(Product product) {
+                return false;
+            }
+
+            @Override
+            public Product remove() {
+                return null;
+            }
+
+            @Override
+            public Product poll() {
+                return null;
+            }
+
+            @Override
+            public Product element() {
+                return null;
+            }
+
+            @Override
+            public Product peek() {
+                return null;
+            }
+        };
         Prev = new ArrayList<>();
         Next = new ArrayList<>();
     }
@@ -38,11 +138,11 @@ public class QueueofProducts implements Observer {
         NumOfProducts = numOfProducts;
     }
 
-    public Stack<Product> getProducts() {
+    public Queue<Product> getProducts() {
         return Products;
     }
 
-    public void setProducts(Stack<Product> products) {
+    public void setProducts(Queue<Product> products) {
         Products = products;
     }
 
